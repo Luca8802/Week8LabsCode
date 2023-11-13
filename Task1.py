cb278d332a87bbfd99f4c8f9ee5e97609bb3d081
Luca8802 <145971885+Luca8802@users.noreply.github.com>
2023-11-13 09:58:47 +0000

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,5 @@
+width = 104.32 #assinging a float value of 104.32 to the variable named width
+height = 15.654 #assinging a float value of 15.654 to the variable named height
+area = width * height #using the '*' operator to multiply the width and height to find the area
+
+print(f"The area of a rectangle with a width of {width} and a height of {height} is {area}") #using the print function to display the area of a rectangle using f-strings to display and use variables
